cb72d3c657f8ad86a48a3b68a41649698185bb46
Ben Reed <ben@benreed.me>
2013-02-13 09:43:56 +0000

    Refactored project into library and demo project

---
+++ Demo/BRViewController.h
@@ -0,0 +1,17 @@
+//
+//  BRViewController.h
+//  BRBluetoothManager
+//
+//  Created by Ben Reed on 06/02/2013.
+//  Copyright (c) 2013 Ben Reed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BRBluetoothManager.h"
+
+@interface BRViewController : UIViewController <BRBluetoothManagerDelegate>
+
+@property (nonatomic, strong) IBOutlet UIButton *sendButton;
+@property (nonatomic, strong) IBOutlet UIButton *connectButton;
+
+@end
